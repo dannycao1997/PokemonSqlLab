15d667e1f1258fed5fc17e75b329eddedf5e4a31
Danny Cao <dannycao1997@yahoo.com>
2024-04-05 11:22:35 -0400

    FINISHED

--- answers/Part 2: Simple Selects and Counts.sql
+++ answers/Part 2: Simple Selects and Counts.sql
@@ -4,13 +4,18 @@
 SELECT * FROM types;
 
 -- What is the name of the pokemon with id 45?
-SELECT * FROM pokemons WHERE id = 45;
+SELECT * FROM pokemons
+WHERE id = 45;
 
 -- How many pokemon are there?
-SELECT COUNT(id) FROM pokemons;
+SELECT COUNT(id)
+FROM pokemons;
 
 -- How many types are there?
-SELECT COUNT(id) FROM types;
+SELECT COUNT(id)
+FROM types;
 
 -- How many pokemon have a secondary type?
-SELECT COUNT(id) FROM pokemons WHERE secondary_type IS NOT NULL;
+SELECT COUNT(id)
+FROM pokemons
+WHERE secondary_type IS NOT NULL;
